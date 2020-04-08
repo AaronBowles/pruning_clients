93b7db481f0d63957c08ee32d8576b56bbe5c66f
AaronBowles <Bowles@Aarons-MBP.attlocal.net>
2020-04-08 17:06:09 -0400

    created additional model fields for clients

--- backend/pruningClients/models.py
+++ backend/pruningClients/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django.contrib.postgres.fields import JSONField
 # Create your models here.
 
-class Clients(models.Model):
+class Client(models.Model):
     lastName = models.CharField(max_length=100)
     firstName = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
@@ -11,4 +11,11 @@ class Clients(models.Model):
     zip = models.IntegerField()
     phone = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-    
+    serviceDate = JSONField(default=dict)
+    pruningHours = JSONField(default=dict)
+    haulHours = JSONField(default=dict)
+    primaryPlants = models.TextField()
+    notes = models.TextField()
+
+    def __str__(self):
+        return self.lastname
