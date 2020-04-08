b3f828e1781a9200f30365b56030a069f2e3d789
AaronBowles <Bowles@Aarons-MBP.attlocal.net>
2020-04-08 17:13:15 -0400

    created client views

--- backend/pruningClients/views.py
+++ backend/pruningClients/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+
 from rest_framework import generics
-from .models import Clients
+from .serializers import ClientSerializer
+from .models import Client
 # Create your views here.
+
+class ClientList(generics.ListCreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+class ProduceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
