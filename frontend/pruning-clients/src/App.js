46d15d10aab8224e9dd3e0832ff83c600b4a5a03
AaronBowles <Bowles@Aarons-MBP.attlocal.net>
2020-04-13 15:25:54 -0400

    began App component

--- frontend/pruning-clients/src/App.js
+++ frontend/pruning-clients/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(state) {
+    super(state)
+    this.state = {
+      clients:""
+    }
+  }
+
+  componentDidMount() {
+    Axios.get('https://pruning-client-backend.herokuapp.com/clients')
+    .then(res => {
+      console.log(res.data);
+      this.setState({cleints: res.data});
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1> Hello</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
